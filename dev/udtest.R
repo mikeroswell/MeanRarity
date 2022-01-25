84c4cf5bab1bf2e5746a5c95dc06751c5f494cc8
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-01-25 13:36:48 -0500

    fiddling

--- dev/udtest.R
+++ dev/udtest.R
@@ -7,21 +7,21 @@ ll <- 1e-10
 lh <- 1
 steps <- 20
 
-lvals <- exp(seq(log(ll), log(lh), length.out=steps))
+lvals <- -exp(seq(log(ll), log(lh), length.out=steps))
 
-for (l in lvals){
-	print("l")
-	print(l)
+for (l in c(lvals, -lvals)){
+	cat("\n")
 	print(down(up(1.1, l), l))
 	print(down(up(2, l), l))
 	print(down(up(10, l), l))
 }
 
+quit()
+
 un <- numeric()
 us <- numeric()
 for (l in lvals){
 	un <- up(10, l)
-	un <- up(10, l, t=0)
 }
 
 

65ffbe09c279d920bdcf1401dda61a5c651c4d9c
mikeroswell <mike.roswell@gmail.com>
2022-01-25 14:02:03 -0500

    tested up and down in rarity function. Might have solved a different precision problem but not the first one that caught my eye.

--- dev/updown.R
+++ dev/updown.R
@@ -20,4 +20,32 @@ down <- function(u, l, t=1e-4, t1=NULL){
 	return((l*u+1)^(1/l))
 }
 
-saveEnvironment()
+
+pfun = function(x, pow){
+  if (pow == 0) return(log(x))
+  r <- sign(pow) * up(x,pow)
+  return(r)
+}
+
+ipfun = function(x, pow){
+  if (pow == 0) return(exp(x))
+  x <- ifelse(sign(pow) * x < 0, 0, x) #added so that ggplot padding doesn't introduce negative values to scale
+  r <- (sign(pow) * down(x, pow))
+  return(r)
+}
+
+# modified rarity for better numerical behavior (precise rarity)
+prec_rarity = function(ab, l, q = NULL){
+  if(!is.null(q)){
+    l = 1-q
+    warning("l has been set to 1-q")
+  }
+  ab = ab[ab != 0]
+  rp = ab/sum(ab)
+  if(l == 0){return(exp(sum(rp * log(1/rp))))}
+  return(down(sum(rp * up(1/rp, l)), l)) # replaces pfun and ipfun with up, down
+}
+
+# test that it gives finite answers
+prec_rarity(c(10,1), l = 296)
+prec_rarity(c(10,1), l = 297)
